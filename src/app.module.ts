cfefd98c7decb34b664e42f93d9a59d945315868
yujo <yujo.dev@gmail.com>
2022-01-11 15:42:55 +0900

    feat: 도메인 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './configs/typeorm.config';
+import { CartsController } from './carts/carts.controller';
+import { CartsService } from './carts/carts.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
+import { CartsModule } from './carts/carts.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeORMConfig)],
+  imports: [
+    TypeOrmModule.forRoot(TypeORMConfig),
+    ProductsModule,
+    CartsModule,
+    OrdersModule,
+  ],
+  controllers: [CartsController, OrdersController, ProductsController],
+  providers: [CartsService, OrdersService, ProductsService],
 })
 export class AppModule {}
