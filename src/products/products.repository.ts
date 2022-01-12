ebc683a8a64e52f8a078754c2778680e24c4684f
yujo <yujo.dev@gmail.com>
2022-01-12 23:06:17 +0900

    refactor: 파일 구조 변경

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
-import { Product } from './entity/product.entity';
+import { Product } from './product.entity';
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
